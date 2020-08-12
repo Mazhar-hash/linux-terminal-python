b2c20db871f790eaa2ea99162479c3cb3744ea65
Your Name <you@example.com>
2020-08-12 17:56:12 -0400

    Basic log in and sign up; no file system or commands

---
+++ v0.1.py
@@ -0,0 +1,50 @@
+import time, random
+iamlordvoldemort = ["thisismypasscode", "iamcoolxxx"]
+#Other user's lists...
+
+#Combining lists
+user_dict = {
+    "iamlordvoldy": iamlordvoldemort
+    #Others ...
+}
+
+
+#Logging in and Signing up ...
+#Maybe make database in other version ???
+# * so that things don't get deleted when you stop the program
+def get_In():
+    global x
+    x = input("> Log in or Sign up?\n")
+    x = x.lower()
+    if x in ["log in", "sign up"]:
+        if x == "log in":
+            x = input("> Please enter your username: \n").lower()
+            if x in user_dict:
+                y = input("> Please enter your password: \n").lower()
+                if y == user_dict[x][0]:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            x = input("> Please enter your username: \n").lower()
+            if x in user_dict:
+                return False
+            else:
+                y = input("> Please enter your password: \n").lower()
+                z = input("> Please enter your computer's name: \n").lower()
+                user_dict[str(x)] = [str(y), str(z)]
+                return True
+    else:
+        get_In()
+# x is username
+while True:
+    if get_In():
+        while True:
+            i = input(str(x) + '@' + str(user_dict[x][1]) + ':~$ ')
+    else:
+        print('There was an error processing your request.')
+        time.sleep(1)
+        print('Please try again.')
+        time.sleep(1)
